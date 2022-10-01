3198d42469369f4a99b64a2f4a771a26e76d7d18
Evgeniy <jenyasereb95@gmail.com>
2022-10-01 16:45:01 +0500

    3

---
+++ Task 1/Task 2/Task 3/Задача 38.cs
@@ -0,0 +1,67 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void FillArray(double [] num )
+{
+ for (int i = 0; i < num.Length; i++)
+{
+    num[i] = new Random().Next(0, 11) + new Random().NextDouble();
+}
+
+}
+    
+void PrintArray(double [] num )
+{
+
+for (int i = 0; i < num.Length; i++)
+{
+    Console.Write(num[i] + " ");
+    
+    
+}
+Console.WriteLine();
+}
+double Max(double [] num)
+{
+    double max = num[0];
+    
+    for (int i = 0; i < num.Length; i++)
+    {
+        if(num[i] > max)
+        {
+            max = num[i];
+        }
+
+    }
+
+    Console.WriteLine("Максимальный элемент массива = " + max);
+          return max;
+}
+  double Min(double [] num)
+
+          {
+            double min = num[0];
+for (int i = 0; i < num.Length; i++)
+    {
+        if(num[i] < min)
+        {
+            min = num[i];
+        }
+
+    }
+    Console.WriteLine("Минимальный элемент массива = " + min);
+    return min;
+}
+void Diff(double Min , double Max)
+{
+    double raz = Min - Max;
+    Console.WriteLine("Разница между максимальным и минимальным значением = " + raz);
+}
+
+double [] numbers =  new double [10];
+
+
+ FillArray(numbers);
+ PrintArray(numbers);
+ double Maximum = Max(numbers);
+ double Minimum = Min(numbers);
+
+ Diff(Maximum , Minimum);
